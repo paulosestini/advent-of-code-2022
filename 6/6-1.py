dfb10ecad84bfce6f33b9fba465e0e214121a43d
Paulo Sestini <24728744+paulosestini@users.noreply.github.com>
2022-12-08 00:51:03 -0300

    Add files via upload

---
+++ 6/6-1.py
@@ -0,0 +1,15 @@
+with open('./6/input.txt') as file:
+    signal = file.read().strip()
+    marker_chars = []
+
+    i = 0
+    while len(marker_chars) != 4:
+        next_char = signal[i]
+        if next_char in marker_chars:
+            char_position = marker_chars.index(next_char)
+            marker_chars = marker_chars[char_position+1:]
+
+        marker_chars.append(next_char)
+        i += 1
+
+    print(i)
